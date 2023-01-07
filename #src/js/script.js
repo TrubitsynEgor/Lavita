18536a75bb9ee11753dbed0780208600748d8c27
TrubitsynEgor <trubitsynwork74@gmail.com>
2023-01-07 18:03:14 +0500

    сделал табы в JS

--- #src/js/script.js
+++ #src/js/script.js
@@ -1,22 +1,48 @@
 'use strict'
 
-const next = document.querySelector('.next');
-const prev = document.querySelector('.prev')
+// const next = document.querySelector('.next');
+// const prev = document.querySelector('.prev')
 
-const swiper = new Swiper('.swiper', {
-	// Optional parameters
-	loop: true,
-	// Navigation arrows
-	navigation: {
-		nextEl: next,
-		prevEl: prev,
-	},
-});
+// const swiper = new Swiper('.swiper', {
+// 	// Optional parameters
+// 	loop: true,
+// 	// Navigation arrows
+// 	navigation: {
+// 		nextEl: next,
+// 		prevEl: prev,
+// 	},
+// });
 
 
-const burgerBtn = document.querySelector('.burger-desctop__btn');
-const burgerMenuDesc = document.querySelector('.burger-desctop')
-burgerBtn.addEventListener('click', (e) => {
-	burgerBtn.classList.toggle('active');
-	burgerMenuDesc.classList.toggle('open');
+// const burgerBtn = document.querySelector('.burger-desctop__btn');
+// const burgerMenuDesc = document.querySelector('.burger-desctop')
+// burgerBtn.addEventListener('click', (e) => {
+// 	burgerBtn.classList.toggle('active');
+// 	burgerMenuDesc.classList.toggle('open');
+// })
+
+
+
+const tabsLink = document.querySelectorAll('.tabs__title-link');
+const tabsBody = document.querySelectorAll('.tabs__body');
+
+
+
+tabsLink.forEach(el => {
+	el.addEventListener('click', () => {
+
+		tabsBody.forEach(el => {
+			el.classList.remove('active')
+		});
+		const content = document.querySelector('#' + el.dataset.tab);
+		content.classList.add('active');
+
+		tabsLink.forEach(el => {
+			el.classList.remove('active')
+		});
+
+		el.classList.add('active')
+
+	})
 })
+
